85532eee33644ef328acc065c6c42c91a51fafa1
tony <tony.gibbs@orasi.com>
2023-02-02 19:41:02 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/OtherTG/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = "Gaurav"
+}
