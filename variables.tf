9bf08045629b720614cfccd5489fbd084847b92d
tony <tony.gibbs@orasi.com>
2023-02-02 21:37:55 +0000

    Added network module

--- variables.tf
+++ variables.tf
@@ -1,75 +1,86 @@
-##############################################################################
-# Variables File
-# 
-# Here is where we store the default values for all the variables used in our
-# Terraform code. If you create a variable with no default, the user will be
-# prompted to enter it (or define it via config file or command line flags.)
-
-variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
+variable "resource_group_name" {
+  description = "The name of an existing resource group to be imported."
+  type        = string
 }
 
-variable "location" {
-  description = "The region where the virtual network is created."
-  default     = "centralus"
+variable "use_for_each" {
+  description = "Use `for_each` instead of `count` to create multiple resource instances."
+  type        = bool
+  nullable    = false
 }
 
 variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  description = "The address space that is used by the virtual network."
+  type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_prefix" {
+variable "address_spaces" {
+  description = "The list of the address spaces that is used by the virtual network."
+  type        = list(string)
+  default     = []
+}
+
+# If no values specified, this defaults to Azure DNS 
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  type        = list(string)
+  default     = []
+}
+
+variable "resource_group_location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+
+variable "subnet_delegation" {
+  description = "`service_delegation` blocks for `azurerm_subnet` resource, subnet names as keys, list of delegation blocks as value, more details about delegation block could be found at the [document](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet#delegation)."
+  type = map(list(object({
+    name = string
+    service_delegation = object({
+      name    = string
+      actions = optional(list(string))
+    })
+  })))
+  default  = {}
+  nullable = false
+}
+
+variable "subnet_enforce_private_link_endpoint_network_policies" {
+  description = "A map with key (string) `subnet name`, value (bool) `true` or `false` to indicate enable or disable network policies for the private link endpoint on the subnet. Default value is false."
+  type        = map(bool)
+  default     = {}
+}
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  type        = list(string)
+  default     = ["subnet1"]
+}
+
+variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
 }
 
-variable "vm_size" {
-  description = "Specifies the size of the virtual machine."
-  default     = "Standard_B1s"
+variable "subnet_service_endpoints" {
+  description = "A map with key (string) `subnet name`, value (list(string)) to indicate enabled service endpoints on the subnet. Default value is []."
+  type        = map(list(string))
+  default     = {}
 }
 
-variable "image_publisher" {
-  description = "Name of the publisher of the image (az vm image list)"
-  default     = "Canonical"
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
+  default = {
+    environment = "dev"
+  }
 }
 
-variable "image_offer" {
-  description = "Name of the offer (az vm image list)"
-  default     = "UbuntuServer"
-}
-
-variable "image_sku" {
-  description = "Image SKU to apply (az vm image list)"
-  default     = "16.04-LTS"
-}
-
-variable "image_version" {
-  description = "Version of the image to apply (az vm image list)"
-  default     = "latest"
-}
-
-variable "admin_username" {
-  description = "Administrator user name for linux and mysql"
-  default     = "hashicorp"
-}
-
-variable "admin_password" {
-  description = "Administrator password for linux and mysql"
-  default     = "Password123!"
-}
-
-variable "height" {
-  default     = "400"
-  description = "Image height in pixels."
-}
-
-variable "width" {
-  default     = "600"
-  description = "Image width in pixels."
-}
-
-variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+variable "vnet_name" {
+  description = "Name of the vnet to create."
+  type        = string
+  default     = "acctvnet"
 }
