fedca54656d1fe2cdadeaaa8530a4cb4a463312a
tony <tony.gibbs@orasi.com>
2023-02-02 21:08:16 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.60.0"
+      source  = "app.terraform.io/OtherTG/network/azurerm"
+      version = "=3.5.0"
     }
   }
 }
